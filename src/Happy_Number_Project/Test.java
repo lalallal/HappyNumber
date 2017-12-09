03594e8f1a1e3ad58c3787049d7c19042cfd2a77
apple <apple@10.181.140.186>
2017-12-09 03:48:03 -0600

    lol

--- src/Happy_Number_Project/Test.java
+++ src/Happy_Number_Project/Test.java
@@ -6,9 +6,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BigNumber a = new BigNumber("104567890232481349184130748107348013478093170498247132984709124701924709132741924701894719374192847");
+		BigNumber a = new BigNumber("1");
 		
-		System.out.println(a.number);
+		System.out.println(a.number.equals("1"));
 		
 	}
 
